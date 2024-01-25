319a2d13c3b13db000fb05bd176fc918d0397ca2
Черепахин Василий <vasi.che@gmail.com>
2024-01-25 21:17:57 +0500

    test Kafka Json deserializer and Jackson Json deserializer

--- src/test/kotlin/ru/perm/v/shopkotlin/kafka_receiver/DeserializerProductExtDTOTest.kt
+++ src/test/kotlin/ru/perm/v/shopkotlin/kafka_receiver/DeserializerProductExtDTOTest.kt
@@ -1,5 +1,6 @@
 package ru.perm.v.shopkotlin.kafka_receiver
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.kafka.support.serializer.JsonDeserializer
@@ -7,11 +8,23 @@ import ru.perm.v.shopkotlin.extdto.ProductExtDTO
 
 class DeserializerProductExtDTOTest {
     @Test
-    fun fromJson() {
+    fun withKafkaJsonDeserializer() {
+        // this used KAFKA dependency
         val deserializer = JsonDeserializer(ProductExtDTO::class.java)
         val json = "{\"n\":10,\"name\":\"NAME_10\",\"groupDtoN\":100}"
 
         val productExtDto = deserializer.deserialize("", json.toByteArray())
+
+        assertEquals(ProductExtDTO(10, "NAME_10", 100), productExtDto)
+    }
+
+    @Test
+    fun withJacksonMapper() {
+        // this used Jackson dependency
+        val mapper = jacksonObjectMapper()
+        val json = "{\"n\":10,\"name\":\"NAME_10\",\"groupDtoN\":100}"
+
+        val productExtDto= mapper.readValue(json, ProductExtDTO::class.java)
 
         assertEquals(ProductExtDTO(10, "NAME_10", 100), productExtDto)
     }
