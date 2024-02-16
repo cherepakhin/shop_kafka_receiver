ec3d887bbd221e2e495d4165ad1925cf504961d2
Черепахин Василий <vasi.che@gmail.com>
2024-02-16 09:51:22 +0500

    generator product_many.json

--- doc/send_many_messages.sh
+++ doc/send_many_messages.sh
@@ -2,15 +2,25 @@
 
 # for test send many messages to queue `product_ext_dto_topic`  and view in console received messages
 # run from ./doc
-max=2
+
+# example:
+# {"n":1,"name":"NAME_1","groupDtoN":1}
+# {"n":1,"name":"NAME_1","groupDtoN":1}
+# {"n":1,"name":"NAME_1","groupDtoN":1}
+
+
 # one way 'for'
 #for i in $(seq 1 $max)
+
 # other way 'for'
-echo > ./product_many.json
+cat /dev/null > ./product_many.json
+
+# generate json for sending to queue
+max=120
 for ((i=1; i < max; i++))
 do
-#    ./run-producer.sh product_ext_dto_topic < ./product.json
   echo "{\"n\":$i,\"name\":\"NAME_$i\",\"groupDtoN\":$i}" >> ./product_many.json
-#  ./run-producer.sh product_ext_dto_topic < ./product_many.json
 done
+
+# send json to queue
 ./run-producer.sh product_ext_dto_topic < ./product_many.json
